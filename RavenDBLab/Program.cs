45376c896094af7a8f6fdffdf36485c738736ca3
sam <evaoosyz@gmail.com>
2018-02-16 15:53:14 +0800

    Lesson 2 - Exercise 1

--- RavenDBLab/Program.cs
+++ RavenDBLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Raven.Client.Documents;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,18 @@ namespace RavenDBLab
     {
         static void Main(string[] args)
         {
+            var documentStore = new DocumentStore
+            {
+                Urls = new string[] { "http://localhost:8080" },
+                Database = "Northwind"
+            };
+
+            documentStore.Initialize();
+            using (var session = documentStore.OpenSession())
+            {
+                var p = session.Load<dynamic>("products/1-A");
+                System.Console.WriteLine(p.Name);
+            }
         }
     }
 }
