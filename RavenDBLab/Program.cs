9637d4b935d09ae24e4df3a092e8d0fe50df9896
sam <evaoosyz@gmail.com>
2018-02-18 18:43:43 +0800

    Lesson 4 - Exercise: Exploring Orders

--- RavenDBLab/Program.cs
+++ RavenDBLab/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Raven.Client.Documents;
+using RavenDBLab.Models;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,55 +13,49 @@ namespace RavenDBLab
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Please, enter an order # (0 to exit): ");
+                string orderNumber = Console.ReadLine();
+                PrintOrder(orderNumber);
+            }
+
+        }
+
+        private static void PrintOrder(string orderNumber)
+        {
             using (var session = DocumentStoreHolder.Store.OpenSession())
             {
-                var p1 = session.Load<Product>("products/1-A");
-                var p2 = session.Load<Product>("products/1-A");
-                //ReferenceEquals(p1, p2) is true
-                Console.WriteLine("ReferenceEquals(p1, p2) is "+ ReferenceEquals(p1, p2));
+                var order = session
+                    .Include<Order>(o => o.Company)
+                    .Include(o => o.Employee)
+                    .Include(o => o.Lines.Select(l => l.Product))
+                    .Load(orderNumber);
 
-                //4.0 好像不支援用index取資料了
-                //Product product = session.Load<Product>(1);
-                //Product[] products = session.Load<Product>(1, 2, 3);
+                if (order == null)
+                {
+                    Console.WriteLine($"Order #{orderNumber} not found.");
+                    return;
+                }
 
-                Dictionary<string, Product> products = session.Load<Product>(new[] {
-                                                            "products/1-A",
-                                                            "products/2-A",
-                                                            "products/3-A"
-                                                        });
+                Console.WriteLine($"Order #{orderNumber}");
 
-                //make two remote calls and this is not a good thing
-                var p = session.Load<Product>("products/1-A");
-                var c = session.Load<Category>(p.Category);
+                var c = session.Load<Company>(order.Company);
+                Console.WriteLine($"Company : {c.Id} - {c.Name}");
 
-                //1) Find a document with the key: products/1-A
-                //2) Read its Category property value.
-                //3) Find a document with that key.
-                //4) Send both documents back to the client.
-                var pp = session.Include<Product>(x => x.Category)
-                                .Load("products/1-A");
+                var e = session.Load<Employee>(order.Employee);
+                Console.WriteLine($"Employee: {e.Id} - {e.LastName}, {e.FirstName}");
 
-                var cc = session.Load<Category>(p.Category);
+                foreach (var orderLine in order.Lines)
+                {
+                    var p = session.Load<Product>(orderLine.Product);
+                    Console.WriteLine($"   - {orderLine.ProductName}," +
+                              $" {orderLine.Quantity} x {p.QuantityPerUnit}");
+                }
             }
         }
     }
 
-    public class Product
-    {
-        public string Name { get; set; }
-        public string Supplier { get; set; }
-        public string Category { get; set; }
-        public string QuantityPerUnit { get; set; }
-        public float PricePerUnit { get; set; }
-        public int UnitsInStock { get; set; }
-        public int UnitsOnOrder { get; set; }
-        public bool Discontinued { get; set; }
-        public int ReorderLevel { get; set; }
-    }
-    public class Category
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
+    
    
 }
