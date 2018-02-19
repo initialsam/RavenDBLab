376aa4b4daf8acf0fa03821557b352aa67af59a2
sam <evaoosyz@gmail.com>
2018-02-19 23:40:39 +0800

    重構 將每個練習 獨立

---
+++ RavenDBLab/Unit1Lesson2.cs
@@ -0,0 +1,49 @@
+﻿using Raven.Client.Documents;
+using RavenDBLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDBLab
+{
+    public class Unit1Lesson2
+    {
+        public static void Exercise1()
+        {
+            var documentStore = new DocumentStore
+            {
+                Urls = new string[] { "http://localhost:8080" },
+                Database = "Northwind"
+            };
+
+            documentStore.Initialize();
+            using (var session = documentStore.OpenSession())
+            {
+                var p = session.Load<dynamic>("products/1-A");
+                System.Console.WriteLine(p.Name);
+            }
+
+        }
+
+        public static void Exercise2()
+        {
+            var documentStore = new DocumentStore
+            {
+                Urls = new string[] { "http://localhost:8080" },
+                Database = "Northwind"
+            };
+
+            documentStore.Initialize();
+            using (var session = documentStore.OpenSession())
+            {
+                var p = session.Load<Product>("products/1-A");
+                System.Console.WriteLine(p.Name);
+                System.Console.WriteLine(p.Supplier);
+                System.Console.WriteLine(p.Category);
+            }
+        }
+    
+    }
+}
---
+++ RavenDBLab/Unit1Lesson3.cs
@@ -0,0 +1,25 @@
+﻿using Raven.Client.Documents;
+using RavenDBLab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RavenDBLab
+{
+    public class Unit1Lesson3
+    {
+        public static void Exercise1()
+        {
+            using (var session = DocumentStoreHolder.Store.OpenSession())
+            {
+                var p = session.Load<Product>("products/1-A");
+                System.Console.WriteLine(p.Name);
+                System.Console.WriteLine(p.Supplier);
+                System.Console.WriteLine(p.Category);
+            }
+
+        }
+    }
+}
